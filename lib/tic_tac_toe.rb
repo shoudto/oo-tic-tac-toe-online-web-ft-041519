59b1aacdeba1c46ff8ac3240e59bca0ef0f97f67
Oscar Shoudt <shoudto@gmail.com>
2019-05-20 18:40:56 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,11 +10,23 @@ class TicTacToe
     [0,3,6],
     [1,4,7],
     [2,5,8],
-    [0,4,8],   
-    [2,4,6]
+    [2,4,6],   
+    [0,4,8]
     ]
   
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+  end 
+  
+  def display_board
+     puts " #{board[0]} | #{board[1]} | #{board[2]} "
+     puts "-----------"
+     puts " #{board[3]} | #{board[4]} | #{board[5]} "
+     puts "-----------"
+     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(string)
+    
   end 
 end 
